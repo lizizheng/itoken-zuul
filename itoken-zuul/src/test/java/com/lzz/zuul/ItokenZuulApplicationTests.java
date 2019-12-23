5ac7da39fca4ac59220f3b1e7050cb6a4142e126
grcavalier92 <982297627@qq.com>
2019-12-23 09:12:20 +0800

    zull

---
+++ itoken-zuul/src/test/java/com/lzz/zuul/ItokenZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItokenZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
